006ee12b1ee9803f0bc8a3dd9f430034ba074f26
Andy <aejefferson@icloud.com>
2025-09-25 15:10:52 +0200

    chore(docs): add resource provider development guide

--- example-monorepo/apps/frontend/types/env.d.ts
+++ example-monorepo/apps/frontend/types/env.d.ts
@@ -11,6 +11,7 @@ declare global {
 
 declare module "cloudflare:workers" {
   namespace Cloudflare {
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     export interface Env extends CloudflareEnv {}
   }
 }
