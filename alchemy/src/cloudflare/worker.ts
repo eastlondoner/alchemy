fe7d9ffabb86bc3cba00c2ef85c52c8ca975ecd4
Andy <aejefferson@icloud.com>
2025-10-10 13:32:18 +0200

    fix bug where some container props like maxInstances were not being passed from Conainer to ContainerApplication

--- alchemy/src/cloudflare/worker.ts
+++ alchemy/src/cloudflare/worker.ts
@@ -1314,15 +1314,10 @@ async function provisionResources<B extends Bindings>(
         ? Promise.all(
             input.containers.map(async (container) => {
               return await ContainerApplication(container.id, {
-                image: container.image,
-                name: container.name,
-                instanceType: container.instanceType,
-                observability: container.observability,
+                ...container,
                 durableObjects: {
                   namespaceId: await getContainerNamespaceId(container),
                 },
-                schedulingPolicy: container.schedulingPolicy,
-                adopt: container.adopt,
                 dev: options.local,
                 ...input.api,
               });
